03c50bd1228ce0b510c53f95cffb33a2eace71d3
Anandpravesh Singh <Asingh4337@conestogac.on.ca>
2024-05-16 14:04:08 -0400

    backend setup complete

---
+++ backend/src/index.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@Prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
+import { Hono } from 'hono';
+
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL:string
+  }
+}>
+
+app.post('/api/v1/signup', (c) => 
+{
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/signin', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Hello Hono!')
+})
+
+export default app
